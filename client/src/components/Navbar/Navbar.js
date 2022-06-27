337ecf9ddadae5dff296b72b9d5410260480de56
Jake Huang <zijianhuang03@gmail.com>
2022-06-26 20:40:43 -0400

    more rows for messages

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,13 +2,22 @@ import React from "react";
 import { AppBar, Typography } from "@material-ui/core";
 import useStyles from "./styles";
 import memories from "../../images/memories.png";
+import Link from "react-router-dom";
 const Navbar = () => {
   const classes = useStyles();
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
-      <Typography className={classes.heading} variant="h2" align="center">
-        Memories
-      </Typography>
+      <div className={classes.brandContainer}>
+        <Typography
+          component={Link}
+          to="/"
+          className={classes.heading}
+          variant="h2"
+          align="center"
+        >
+          Memories
+        </Typography>
+      </div>
       <img
         className={classes.image}
         src={memories}
