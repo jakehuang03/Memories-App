e8c0ab97a70b406f69e0890d192cd09ecc8d960a
Jake Huang <zijianhuang03@gmail.com>
2022-07-07 14:00:00 -0400

    pagination

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
+import PostDetails from './components/PostDetails/PostDetails';
 
 import { Container } from '@material-ui/core';
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
@@ -15,7 +16,7 @@ const App = () => {
         <Navbar />
         <Switch>
           <Route path="/" exact component={() => <Redirect to="/posts"/>}/>
-          <Route path="/auth" exact component={Home}/>
+          <Route path="/posts" exact component={Home}/>
           <Route path="/posts/search" exact component={Home} />
           <Route path="/posts/:id" exact component={PostDetails} />
           <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/posts" />)} />
---
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostDetails = () => {
+    return (
+        <di>
+            POST DETAILS
+        </di>
+    )
+}
+
+export default PostDetails;
