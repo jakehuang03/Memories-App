7498177ad9b8264e4679918fddd8fc31c30d3ab2
Jake Huang <zijianhuang03@gmail.com>
2022-07-11 17:42:11 -0400

    correctly shows posts in a page

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,10 +1,10 @@
 import * as api from "../api/index.js";
 import { FETCH_ALL, FETCH_BY_SEARCH, UPDATE, DELETE, CREATE } from "../constants/actionTypes";
 
-export const getPosts = () => async (dispatch) => {
+export const getPosts = (page) => async (dispatch) => {
   try {
-    const { data } = await api.fetchPosts();
-    dispatch({ type: FETCH_ALL, payload: data });
+    const { data: { data, currentPage, numberOfPages } } = await api.fetchPosts(page);
+    dispatch({ type: FETCH_ALL, payload: {data, currentPage, numberOfPages} });
   } catch (error) {
     console.log(error.message);
   }
