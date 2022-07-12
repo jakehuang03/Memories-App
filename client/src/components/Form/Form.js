5e86fa60bed9fca891195dfcb541ba9280260566
Jake Huang <zijianhuang03@gmail.com>
2022-07-12 13:20:38 -0400

    add elevation

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -45,7 +45,7 @@ const Form = ({ currentId, setCurrentId }) => {
 
   if (!user?.result?.name && !user?.name) {
     return (
-      <Paper className={classes.paper}>
+      <Paper className={classes.paper} elevation={6}>
         <Typography variant="h6" align="center">
           Please Sign In to create your own memories and like other's memories.
         </Typography>
@@ -54,7 +54,7 @@ const Form = ({ currentId, setCurrentId }) => {
   }
 
   return (
-    <Paper className={classes.paper}>
+    <Paper className={classes.paper} elevation={6}>
       <form
         autoComplete="off"
         noValidate
