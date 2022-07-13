dd652e6060a19b2a40429bda7a63fa106212d0d3
Jake Huang <zijianhuang03@gmail.com>
2022-07-13 17:36:41 -0400

    add width for suggested post pictures

--- client/src/components/PostDetails/PostDetails.jsx
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -71,7 +71,7 @@ const PostDetails = () => {
                     <Typography gutterBottom variant="subtitle2">{name}</Typography>
                     <Typography gutterBottom variant="subtitle2">{message}</Typography>
                     <Typography gutterBottom variant="subtitle1">Likes: {likes.length}</Typography>
-                    <img src={selectedFile} width="200px" />
+                    <img src={selectedFile} width="200px" height="200px"/>
                  </div>
                 ))}
             </div>
