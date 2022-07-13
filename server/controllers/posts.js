bc0485afcd230889015186d6f933645745ef109b
Jake Huang <zijianhuang03@gmail.com>
2022-07-13 14:57:21 -0400

    set posts in a page to 6

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -15,7 +15,7 @@ export const getPost = async (req,res) => {
 export const getPosts = async (req,res) => {
     const {page} = req.query;
     try{
-        const LIMIT = 8;
+        const LIMIT = 6;
         const startIndex = (Number(page)-1)*LIMIT;
         const total = await PostMessage.countDocuments({});
         const posts = await PostMessage.find().sort({_id: -1}).limit(LIMIT).skip(startIndex);
