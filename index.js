96dba2c13f98b997f90a04290a80bb0a78a5642b
Jake Huang <zijianhuang03@gmail.com>
2022-07-18 16:00:52 -0400

    fix bug

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use("/api/user", userRoutes);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('*', (req,res) => {
+  app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, 'client/build/index.html'));
   });
 }
